97f063aa851c21dd1fb7cab6662bd3d16dbdbe18
NinaGundersen <ninamg18@student.uia.no>
2023-11-20 14:03:54 +0100

    outline

--- images/js/nina.js
+++ images/js/nina.js
@@ -1,10 +1,10 @@
-import { rectangle, circle, triangle } from  ”./figures.js”;
+import { rectangle, circle, triangle, circleevenodd } from  ”./figures.js”;
 
 function canvasnina() {
 	rectangle("silver", 0, 0, 90, 90);
-	circle("purple", 45, 80, 20); - outline
+	circleevenodd("purple", 45, 80, 20, 43, 78, 18);
 	circle("blue", 10, 10, 20); 
-	circle("red", 45, 50, 40); - outline
+	circleevenodd("red", 45, 50, 40, 43, 48, 38);
 	circle("dark-green", 80, 10, 20);
 }
 
@@ -13,9 +13,9 @@ canvasnina();
 
 function svgnina() {
 	rectanglesvg("silver", "0", "0", "90", "90");
-	circlesvg("purple", "45", "80", "20"); - outline
+	circlessvg("purple", "45", "80", "20");
 	circlesvg("blue", "10", "10", "20");
-	circlesvg("red", "45", "50", "40"); - outline
+	circlesvg("red", "45", "50", "40");
 	circlesvg("dark-green", "80, 10, 20");
 
 }
