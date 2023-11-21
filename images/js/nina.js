29a7372df0d13c5cc8226a98140b89c8590699d6
NinaGundersen <ninamg18@student.uia.no>
2023-11-21 14:46:56 +0100

    fikse figurer

--- images/js/nina.js
+++ images/js/nina.js
@@ -1,22 +1,22 @@
 import { rectangle, circle, triangle, circleevenodd } from  ”./figures.js”;
 
 function canvasnina() {
-	rectangle("silver", 0, 0, 90, 90);
-	circleevenodd("purple", 45, 80, 20, 43, 78, 18);
-	circle("blue", 10, 10, 20); 
-	circleevenodd("red", 45, 50, 40, 43, 48, 38);
-	circle("dark-green", 80, 10, 20);
+	rectangle("bisque", 0, 0, 90, 90);
+	circleevenodd("purple", 45, 70, 20, 45, 70, 18);
+	circle("blue", 20, 20, 20); 
+	circleevenodd("red", 45, 50, 40, 44, 50, 38);
+	circle("dark-green", 70, 20, 20);
 }
 
 canvasnina();
 
 
 function svgnina() {
-	rectanglesvg("silver", "0", "0", "90", "90");
-	ekstracirclesvg("purple", "45", "80", "20", "43", "78", "18");
-	circlesvg("blue", "10", "10", "20");
-	ekstracirclesvg("red", "45", "50", "40", "43", "48", "38");
-	circlesvg("dark-green", "80, 10, 20");
+	rectanglesvg("bisque", "0", "0", "90", "90");
+	ekstracirclesvg("purple", "45", "70", "20", "transparent", "2");
+	circlesvg("blue", "20", "20", "20");
+	ekstracirclesvg("red", "45", "50", "40", "transparent", "2");
+	circlesvg("dark-green", "70, 20, 20");
 
 }
 
