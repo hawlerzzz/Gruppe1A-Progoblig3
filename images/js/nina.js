fdbf735ac00d82f7ddf3f5336b36fed8822dfc2a
JonAarebakk <jonea@student.uia.no>
2023-11-21 15:09:49 +0100

    okay

--- images/js/nina.js
+++ images/js/nina.js
@@ -4,7 +4,7 @@ function canvasnina() {
 	rectangle("bisque", 0, 0, 90, 90);
 	circleevenodd("purple", 45, 70, 20, 45, 70, 18);
 	circle("blue", 20, 20, 20); 
-	circleevenodd("red", 45, 50, 40, 44, 50, 38);
+	circleevenodd("red", 45, 50, 40, 45, 50, 38);
 	circle("dark-green", 70, 20, 20);
 }
 
@@ -16,7 +16,7 @@ function svgnina() {
 	ekstracirclesvg("purple", "45", "70", "20", "transparent", "2");
 	circlesvg("blue", "20", "20", "20");
 	ekstracirclesvg("red", "45", "50", "40", "transparent", "2");
-	circlesvg("dark-green", "70, 20, 20");
+	circlesvg("dark-green", "70", "20", "20");
 
 }
 
