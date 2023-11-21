6cdc76800dec0accd92f124a8a258b4d3d96bd45
Hawler A. Mohammed <Hawleram@uia.no>
2023-11-21 15:34:35 +0100

    Merge branch 'main' into hawler

---
+++ images/js/ole.js
@@ -0,0 +1,18 @@
+import { rectangle, circle, triangle, rectanglesvg, circlesvg, trianglesvg } from "./figures.js";
+
+function canvasole() {
+    rectangle("black", 0, 0, 100, 100);
+    circle("blue", 20, 25, 10);
+    circle("blue", 60, 25, 10);
+    triangle("red", 40, 40, 60, 40, 50, 30);
+}
+canvasole();
+
+function svgole() {
+    rectanglesvg("black", "0", "0", "100", "100");
+    circlesvg("blue", "20", "25", "10");
+    circlesvg("blue", "60", "25", "10");
+    trianglesvg("red", "40, 40 60, 40 50, 30");
+}
+
+svgole();
