8716fabd199e9bb2bdd574719ab042f2f26045fa
Hawler A. Mohammed <Hawleram@uia.no>
2023-11-21 16:33:41 +0100

    fikset js feil for 1000 gang.

--- images/js/hawler.js
+++ images/js/hawler.js
@@ -1,4 +1,4 @@
-import {rectangle, circle } from "./figures.js";
+import {rectangle, circle, rectanglesvg, circlesvg } from "./figures.js";
 
 function canvashawler(){
     rectangle("black", 0, 0, 100, 100);
@@ -6,10 +6,17 @@ function canvashawler(){
     circle("yellow", 20, 22, 10);
     circle("yellow", 60, 22, 10);
     circle("yellow", 50, 50, 5);
-
 }
 
 canvashawler();
 
 
+function svghawler() {
+    rectanglesvg("black", "0", "0", "100", "100");
+    rectanglesvg("yellow", "25", "80", "50", "5");
+    circlesvg("yellow", "20", "22", "10");
+    circlesvg("yellow", "60", "22", "10");
+    circlesvg("yellow", "50", "50", "5");
+}
 
+svghawler();
