324c7e71001eefaf791952e083c59dcfbc79b2f5
Hawler A. Mohammed <Hawleram@uia.no>
2023-11-21 15:25:07 +0100

    fikset js kode til pyret figur i canvas API. fikset diverse elementer i medlemssidene og hovedsiden.

---
+++ images/js/hawler.js
@@ -0,0 +1,9 @@
+import {rectangle, circle, circlesvg, rectanglesvg, triangle} from "./figures.js";
+
+function canvasjon(){
+    rectangle("yellow", 25, 80,50, 5);
+    circle("yellow", 10, 12, 10);
+    circle("yellow", 70, 12, 10);
+    circle("yellow", 45, 45, 5);
+    rectangle("black", 100, 100, 30);
+    }
