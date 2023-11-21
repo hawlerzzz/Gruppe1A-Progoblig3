ef9676355b035859fcde0c44329682b22be21850
oleols1 <144025972+oleols1@users.noreply.github.com>
2023-11-21 15:39:59 +0100

    Oppdatert størrelse og kordinater til figuren

--- images/js/ole.js
+++ images/js/ole.js
@@ -2,18 +2,18 @@ import { rectangle, circle, triangle, rectanglesvg, circlesvg, trianglesvg } fro
 
 function canvasole(): {
     rectangle("black", 0, 0, 100, 100);
-    rectangle("green", 60, 75, 30, 10);
-    circle("blue", 40, 25, 10);
-    circle("blue", 80, 25, 10);
+    rectangle("green", 35, 75, 30, 10);
+    circle("blue", 70, 25, 10);
+    circle("blue", 30, 25, 10);
     triangle("red", 40, 40, 60, 40, 50, 30);
 }
 canvasole();
 
 function svgole() {
     rectanglesvg("black", "0", "0", "100", "100");
-    rectangle("green", "60", "75", "30", "10");
-    circlesvg("blue", "40", "25", "10");
-    circlesvg("blue", "80", "25", "10");
+    rectangle("green", "35", "75", "30", "10");
+    circlesvg("blue", "30", "25", "10");
+    circlesvg("blue", "70", "25", "10");
     trianglesvg("red", "40, 40 60, 40 50, 30");
 }
 
